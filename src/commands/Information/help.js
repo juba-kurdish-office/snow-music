d454a499525169ef92e49313b8991fcf0564a45d
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-01-24 22:46:24 +0300

    Update help.js

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -1,21 +1,22 @@
-const { MessageEmbed, MessageActionRow, MessageButton, CommandInteraction, Client } = require("discord.js");
-
+const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "help",
-    description: "Replies with help on how to use this bot",
+    category: "Information",
+    aliases: [ "h" ],
+    description: "Return all commands, or one specific command",
+    args: false,
+    usage: "",
+    permission: [],
     owner: false,
+ execute: async (message, args, client, prefix) => {
+      const embed = new MessageEmbed()
 
-
-    run: async (client, interaction, prefix) => {
-        await interaction.deferReply({
-          ephemeral: false
-        });
-  const embed = new MessageEmbed()
     .setTitle("**Help - Menu**")
-    .addField("<a:clever:934459249470357514>İnformation","`setprefix`, `invite`, `status`") 
-    .addField("<a:emoji_110:934441822233722951>Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
-    .addField("<a:emoji_1:928719172094001175>Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
+    .addField("İnformation","`setprefix`, `invite`, `status`") 
+    .addField("Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
+    .addField("Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
     .setColor("RANDOM")
-   await interaction.followUp({embeds: [embed]})
+
+    message.channel.send({ embeds: [embed] }); 
   }
 }
