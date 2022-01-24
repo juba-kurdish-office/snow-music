7e6ed1ac3793f477ba071e94be8bc35c99cc55e5
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-01-24 22:44:52 +0300

    Update help.js

--- src/slashCommands/Information/help.js
+++ src/slashCommands/Information/help.js
@@ -12,7 +12,7 @@ module.exports = {
         });
   const embed = new MessageEmbed()
     .setTitle("**Help - Menu**")
-    .addField("<a:clever:934459249470357514>","`setprefix`, `invite`, `status`") 
+    .addField("<a:clever:934459249470357514>information","`setprefix`, `invite`, `status`") 
     .addField("<a:emoji_110:934441822233722951>Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
     .addField("<a:emoji_1:928719172094001175>Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
     .setColor("RANDOM")
