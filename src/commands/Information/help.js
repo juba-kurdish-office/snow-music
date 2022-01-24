7bd2cba1038d56ee1810dc4ee6e97bf0df4f59e9
Black Man <80176569+zeko628@users.noreply.github.com>
2022-01-24 20:31:00 +0300

    Update help.js

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -11,8 +11,7 @@ module.exports = {
  execute: async (message, args, client, prefix) => {
       const embed = new MessageEmbed()
 
-       .setTitle("**Carlo - Help Menu**")
-       .setDescription(`Created By <@637299944939585576> if you need help join our [Community](https://discord.gg/gGzf9t3d6G)`)
+    .setTitle("**Help - Menu**")
     .addField("İnformation","`setprefix`, `invite`, `status`") 
     .addField("Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
     .addField("Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
